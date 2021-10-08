e75de3193d3d49e42fb496a399023022f9ec1b2a
Izabela Lage <izabelalagelegal@hotmail.com>
2021-10-08 16:11:47 -0300

    Chute um numero
    Exemplo de  chute de um numero

---
+++ ChuteNumero.py
@@ -0,0 +1,33 @@
+# Chute o Número
+import random
+
+class ChuteNumero:
+    def __init__(self):
+        self.valorAleatorio = 0
+        self.valorMin = 1
+        self.valorMax = 100
+        self.tentarNovamente = True
+
+    def Iniciar(self):
+        self.GerarNumero()
+        self.PedirValorAleatorio()
+        while self.tentarNovamente == True:
+            if int(self.valorChute) > self.valorAleatorio:
+                print('Chute um valor mais baixo!')
+                self.PedirValorAleatorio()
+            elif int(self.valorChute) < self.valorAleatorio:
+                print('Chute um valor mais alto!')
+                self.PedirValorAleatorio()
+            if int(self.valorChute) == self.valorAleatorio:
+                self.tentarNovamente = False
+                print('Parabéns você acertou !!!!!')
+
+    def PedirValorAleatorio(self):
+        self.valorChute = input('Chute um número: ')
+
+
+    def GerarNumero(self):
+      self.valorAleatorio = random.randint(self.valorMin,self.valorMax)
+
+chute = ChuteNumero()
+chute.Iniciar()
